c92aa3dc1d55037c9d6f27ec4169fa8a56177dc8
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-11-19 03:02:35 -0300

    boton volver

--- Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/restaurante/ControladorActualizarDatosRest.java
+++ Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/restaurante/ControladorActualizarDatosRest.java
@@ -53,6 +53,9 @@ public class ControladorActualizarDatosRest implements ApplicationContextAware {
 
 	@FXML
 	private Button btnCargarLogo;
+	
+	@FXML
+	private Button btnVolver;
 
 	@FXML
 	private ImageView imgView;
@@ -216,21 +219,21 @@ public class ControladorActualizarDatosRest implements ApplicationContextAware {
 		logoAGuardar = fileContent;
 	}
 
-	// @FXML
-	// void volverAMenu(ActionEvent event) throws IOException {
-	// Stage stage = new Stage();
-	// Parent root = null;
-	// FXMLLoader fxmlLoader = new FXMLLoader();
-	// fxmlLoader.setControllerFactory(applicationContext::getBean);
-	// if(event.getSource()==btnVolverAMenu) {
-	// stage = (Stage) btnGuardarDatos.getScene().getWindow();
-	// root = fxmlLoader.load(
-	// ControladorActualizarDatosRest.class.getResourceAsStream("MenuPrincipal.fxml"));
-	// }
-	// Scene scene = new Scene(root);
-	// stage.setScene(scene);
-	// stage.show();
-	// }
+	 @FXML
+	 void volverAlMenu(ActionEvent event) throws IOException {
+	 Stage stage = new Stage();
+	 Parent root = null;
+	 FXMLLoader fxmlLoader = new FXMLLoader();
+	 fxmlLoader.setControllerFactory(applicationContext::getBean);
+	 if(event.getSource()==btnVolver) {
+	 stage = (Stage) btnGuardarDatos.getScene().getWindow();
+	 root = fxmlLoader.load(
+	 ControladorActualizarDatosRest.class.getResourceAsStream("MenuPrincipalRest.fxml"));
+	 }
+	 Scene scene = new Scene(root);
+	 stage.setScene(scene);
+	 stage.show();
+	 }
 
 	@FXML
 	void initialize() {
